5897329c1ea6f60f8c09b47a1175219d9dd3175a
Yash <ygupta24@wisc.edu>
2026-09-17 12:55:35 -0500

    Added HW01: task4.sh and task6.cpp

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=FirstSlurm
-#SBATCH --output=FirstSlurm.out
-#SBATCH --error=FirstSlurm.err
-#SBATCH --cpus-per-task=2
+#SBATCH -p instruction
+#SBATCH -t 0-00:05:00
+#SBATCH -J FirstSlurm
+#SBATCH -o FirstSlurm.out
+#SBATCH -e FirstSlurm.err
+#SBATCH -c 2
 
 hostname
